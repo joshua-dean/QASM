4b7640a2ee276f2cd2eb8661b117f9ef9bb0c453
joshua-dean <joshua.dean@astrinbio.com>
2024-01-08 14:22:53 -0600

    Attempt to add lambda API to cdk stack.

--- cdk-backend/app.py
+++ cdk-backend/app.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 import aws_cdk as cdk
-
 from stacks.qasm_backend_stack import QASMBackendStack
+from pathlib import Path
+
+REPO_ROOT = Path(__file__).parent.parent
 
 app = cdk.App()
-QASMBackendStack(
+qasm_backend_stack = QASMBackendStack(
     app,
     "QASMBackendStack",
+)
+
+# .env file generation
+env_file_path = REPO_ROOT / "react-frontend" / "cdk.env"
+env_file_path.touch()
+env_file_path.write_text(
+    f"REACT_APP_API_URL={qasm_backend_stack.lambda_api_url}\n"
 )
 
 app.synth()
--- cdk-backend/stacks/qasm_backend_stack.py
+++ cdk-backend/stacks/qasm_backend_stack.py
@@ -1,25 +1,43 @@
 from aws_cdk import (
     Stack,
-    aws_s3 as s3
+    aws_s3 as s3,
+    aws_apigatewayv2 as apigw_v2,
 )
 from constructs import Construct
+
 
 class QASMBackendStack(Stack):
     """QASM backend stack."""
 
-    def __init__(
-        self,
-        scope: Construct,
-        construct_id: str,
-        **kwargs
-    ) -> None:
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         """Init."""
         super().__init__(scope, construct_id, **kwargs)
-        
+
         # Lambda bucket
         self.lambda_bucket = s3.Bucket(
-            self,
-            "LambdaBucket",
-            block_public_access=s3.BlockPublicAccess.BLOCK_ALL
+            self, "LambdaBucket", block_public_access=s3.BlockPublicAccess.BLOCK_ALL
         )
-        
+
+        self.lambda_api = apigw_v2.HttpApi(
+            self,
+            "LambdaAPI",
+            cors_preflight=apigw_v2.CorsPreflightOptions(
+                allow_origins=["*"],
+                allow_methods=[
+                    apigw_v2.CorsHttpMethod.OPTIONS,
+                    apigw_v2.CorsHttpMethod.PUT,
+                    apigw_v2.CorsHttpMethod.POST,
+                    apigw_v2.CorsHttpMethod.GET,
+                ],
+                allow_headers=["*"],
+            ),
+        )
+
+        self.lambda_api_stage = apigw_v2.HttpStage(
+            self,
+            "LambdaAPIStage",
+            stage_name="prod",
+            auto_deploy=True,
+            http_api=self.lambda_api,
+        )
+        self.lambda_api_url = self.lambda_api.url
